ccd4cd7e00c7d7364299544bb3e92adcd2a82c69
HasanAhmedNoyon <mohammedhasan9047@gmail.com>
2021-07-26 17:17:46 +0600

    1

--- chapter-one/operator.js
+++ chapter-one/operator.js
@@ -1,3 +1,4 @@
+// Part One : 1
 /* Arithmnatic Operator */
 // + - * / % ++ --
 
@@ -59,6 +60,76 @@
 //output should be 9
 
 
+//Part Two : 2
+/* Assignment Operator */
+// = 
+//used for assign value into a variable. Suppose 
+// var a = 10
+//Here value 10 is assigned into variable name (a)
+
+
+//Part Three : 3
+/* Combined: Assignment and Arithmetic Operator */
+// += -= *= /= 
+
+// Plus Assignement Operator += 
+// var a = 10
+// var b = 10
+// We can simply add both value and assigned into variable a 
+// console.log(a += b)
+// console.log(a)
+// value of variable named a should be 20
+
+
+//Minus Assignemnt  Operator -=
+// var a = 20
+// var b = 10
+// console.log(a -= b)
+// console.log(a)
+
+
+//Multiply Assignment Operator *=
+// var a = 10
+// var b = 100
+// console.log(a *= b)
+// console.log(a)
+
+//Divide Assignment Operator *=
+// var a = 100
+// var b = 10
+// console.log(a /= b)
+// console.log(a)
+
+//Modulous Assignment Operator *=
+// var a = 100
+// var b = 13
+// console.log(a %= b)
+// console.log(a)
 
 
 
+
+//Part Four : 4
+//Comparison Operator 
+/* 
+value Equal == 
+value Not Equal != 
+Data type Equal === 
+Data Type Not Equal !=== 
+value Less Then < 
+Value Greater Then > 
+value Equal or Less Then <=
+value Equal or Greater Then >=
+*/
+//Value Equal 
+// var a = 10
+// var b = 20
+// var c = "Hasan Ahmed"
+// console.log(a == b) //false
+// console.log(a != b) // true
+// console.log(a === b) // true 
+// console.log(a !== c) // true
+// console.log(a > b) //false
+// console.log(a < b) //ture
+// console.log(a <= b) //false 
+// console.log(a >= b) // false
